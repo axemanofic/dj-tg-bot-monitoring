998b8089d5fdf9efab39e0edd899d5aa77955e43
Roman Sotnikov <romasotnikov12345@gmail.com>
2022-05-01 23:21:03 +0300

    fix name app

--- dj_tg_bot_monitoring/apps.py
+++ dj_tg_bot_monitoring/apps.py
@@ -4,9 +4,9 @@ from django.core.checks import Tags, register
 from .checks import check_settings
 
 
-class DjangoBotExceptionsConfig(AppConfig):
+class DjangoTelegramBotMonitoringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_bot_exceptions'
+    name = 'dj_tg_bot_monitoring'
 
     def ready(self) -> None:
         register(Tags.security)(check_settings)
