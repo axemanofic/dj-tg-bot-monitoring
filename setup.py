c4d7398d19ccf38880097039332c266c87f14c6c
Roman Sotnikov <romasotnikov12345@gmail.com>
2022-05-01 17:40:45 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dj-tg-bot-monitoring',
+    version='0.1.0',
+    packages=[''],
+    package_dir={'': 'dj_tg_bot_monitoring'},
+    url='https://github.com/axemanofic/dj-tg-bot-monitoring/tree/master',
+    license='MIT',
+    author='Roman Sotnikov',
+    author_email='romasotnikov12345@gmail.com',
+    description='Exception monitoring in django using telegram bot'
+)
