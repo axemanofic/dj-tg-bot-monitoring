c936c8cf65ed72d0ec20a8c4678704d54ae9931b
Roman Sotnikov <romasotnikov12345@gmail.com>
2022-05-04 13:29:28 +0300

    add parametr `parse_mode`

--- dj_tg_bot_monitoring/utils.py
+++ dj_tg_bot_monitoring/utils.py
@@ -3,10 +3,11 @@ from .conf import configuration
 from .consts import TELEGRAM_METHOD
 
 
-def send_message(message: str):
+def send_message(message: str, parse_mode="HTML"):
     data = {
         "text": message,
-        "chat_id": ""
+        "chat_id": "",
+        "parse_mode": parse_mode
     }
     chats = configuration.TELEGRAM_BOT.get('CHATS', set())
     for chat_id in chats:
