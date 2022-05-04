5e6e774afad925c517517ef74270c311804d6d55
Roman Sotnikov <romasotnikov12345@gmail.com>
2022-05-04 12:11:00 +0300

    add file decorators.py

---
+++ dj_tg_bot_monitoring/decorators.py
@@ -0,0 +1,11 @@
+import traceback
+from utils import send_message
+
+
+def report_bug(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func()
+        except Exception as e:
+            send_message(str(traceback.format_exc()))
+        return wrapper
